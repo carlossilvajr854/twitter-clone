f1aac8b3f42dde2e1845b3ac2991cb84af4de083
carlossilvajr854 <carlossilvajr854@gmail.com>
2024-08-26 20:26:26 -0300

    Ajuste

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'app/miniframework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'db0c0a7cd0d62af0c75ea61827a9d4c1b00da652',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'app/miniframework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'db0c0a7cd0d62af0c75ea61827a9d4c1b00da652',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
